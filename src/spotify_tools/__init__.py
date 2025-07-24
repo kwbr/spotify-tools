f22c47e2974bce423943d056844b0bd527c6334d
Kai Weber <kai.weber@glorybox.de>
2025-07-24 21:04:59 +1000

    Change version to be dynamic

--- src/spotify_tools/__init__.py
+++ src/spotify_tools/__init__.py
@@ -3,6 +3,4 @@ Spotify Tools - Collection of helpers to enhance music
 listening experience with Spotify.
 """
 
-import importlib.metadata
-
-__version__ = importlib.metadata.version("spotify-tools")
+__version__ = "0.9"
